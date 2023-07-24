38815df1b7a6a9da95b48e68f7b05570cd130f1b
William-Nimitz <mingchaitest27@gmail.com>
2023-07-24 01:02:26 -0400

    add clipboard

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,7 @@
 (function($) {
     const Referrals = {
         loading: true,
+        selectedReferral: null,
         mockData: {
             referrals: [
                 {
@@ -248,10 +249,6 @@
                     status: "Pending",
                     payout: 125
                 }
-            ],
-            shareEmails: [
-                "test@test.com",
-                "test1@test.com"
             ]
         },
 
@@ -326,10 +323,13 @@
                     Referrals.loading = true;
                 }
                 referralsTable.html(tbody);
-            }, 5000);
+            }, 100000000000000);
 
+            Referrals.initializeToolTip();
             $(document).on("click", ".referrals-table table tbody tr", Referrals.selectReferral);
-            // $(document).on("click", ".editItem", Referrals.editItem);
+            $(document).on("click", ".selected-referral-link-button", Referrals.confirmReferral);
+            $(document).on("click", ".selected-referral-link-wrapper", Referrals.copyLinkToClipboard);
+            $(document).on("click", ".remove-email-from-share", Referrals.removeEmailFromShare);
         },
 
         selectReferral: function(e) {
@@ -339,6 +339,51 @@
             $(".selected-referral-payout").text(referral?.payout || "0.00");
             $(".selected-referral-description").text(referral?.description || "");
             $(".selected-referral-link").text(referral?.link || "");
+
+            Referrals.selectedReferral = referral;
+        },
+
+        confirmReferral: function(e) {
+            // edit madal content 
+            const referral = Referrals.selectedReferral;
+            const options = {
+                backdrop: 'static',
+                keyboard: false
+            };
+            const confirmModal = new bootstrap.Modal(document.getElementById('confirm-modal'), options);
+            confirmModal.show()
+        },
+
+        removeEmailFromShare: function(e) {
+            const removed = $(this).closest(".send-to");
+            removed.remove();
+        },
+
+        copyLinkToClipboard: async function(e) {
+            const link = $(".selected-referral-link").text();
+            const toolTipComponent = $(this);
+            try {
+                navigator.clipboard.writeText(link);
+
+                const options = {
+                    trigger: 'manual',
+                    placement: 'top',
+                    title: 'Copied to clipboard',
+                    delay: { "show": 500, "hide": 500 }
+                };
+                const tooltip = new bootstrap.Tooltip(toolTipComponent, options);
+                tooltip.show();
+                setTimeout(() => tooltip.hide(), 1000);
+            } catch (error) {
+                console.log(err.message);
+            }
+        },
+
+        initializeToolTip: function() {
+            // const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+            // const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            //     return new bootstrap.Tooltip(tooltipTriggerEl);
+            // });
         }
     };
     
